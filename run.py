bac8adf018a17cd0e6863ee442c2eb90557bc6e4
aliasgharmirhshai <lmyrshahy3@gmail.com>
2023-02-03 17:35:44 +0330

    Ping [Check Size limit]

--- run.py
+++ run.py
@@ -1,17 +1,23 @@
 import os
 import sys
 
-host = sys.argv[1]
+def command_handler():
+    host = sys.argv[2]
 
-if 'http' in host:
-    print("Dont Enter Https or Http")
-else:
+    if sys.argv[1] == "-P":
+        ping(host)
+    elif sys.argv[1] == '-Ps':
+        packet_size(host)
+    else:
+        print("Command Not Found")
+
+def ping(host):
     for i in range(1,54):
         get_ttl = os.popen(f"ping {host} -i {i} -n 1").read().split()
         ping = get_ttl[12]
         ip = get_ttl[2]
         expire_ip = get_ttl[10]
-        
+            
         if 'in' in get_ttl[13]:
             print(f"EXPIERD = {i} * On = {expire_ip}")
         elif 'for' in get_ttl[13]:
@@ -21,4 +27,22 @@ else:
             break
         else:  
             print(get_ttl[13])
-  
+
+def packet_size(host):
+    for i in range(0,5000, 100):
+        get_size = os.popen(f"ping {host} -l {i}").read().split()
+        ip = get_size[2]
+        time = get_size[12]
+        timeout = get_size[13]
+        
+        if timeout == 'out.':
+            print(f"\nEnd Sending Packet in {i}Byte")
+            break
+        else:
+            print(f"{ip}   PACKET SZIE = {i}Byte   {time}")
+
+if __name__ == '__main__':
+    try:
+        command_handler()
+    except KeyboardInterrupt:
+        print('\nGoodBy...')
